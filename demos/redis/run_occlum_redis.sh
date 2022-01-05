5e86a49d974362987781d2fc10683d17e5348911
Hui, Chunyang <sanqian.hcy@antfin.com>
2022-01-05 06:53:03 +0000

    [demos] Change back to IPv6 in redis demo

--- demos/redis/run_occlum_redis.sh
+++ demos/redis/run_occlum_redis.sh
@@ -18,4 +18,7 @@ copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
 
 occlum build
 # 3. Run redis server
-occlum run /bin/redis-server /etc/redis.conf &
+# Default IPv6:
+occlum run /bin/redis-server --save "" --appendonly no &
+# With ipv4:
+#occlum run /bin/redis-server /etc/redis.conf &
