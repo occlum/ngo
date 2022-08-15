7cbcc945a7ba903d3c256bf9080b4ff9917bdad3
github-action-benchmark <github@users.noreply.github.com>
2022-08-15 14:47:15 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 49a55608604681e7fbf1d9ad49cce3623b9b1b1e

---
+++ dev/benchmarks/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1660546035140,
+  "repoUrl": "https://github.com/occlum/ngo",
+  "entries": {
+    "Iperf3 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "geding.lq@antgroup.com",
+            "name": "LI Qing",
+            "username": "liqinggd"
+          },
+          "committer": {
+            "email": "volcano.dr@hotmail.com",
+            "name": "volcano",
+            "username": "volcano0dr"
+          },
+          "distinct": true,
+          "id": "49a55608604681e7fbf1d9ad49cce3623b9b1b1e",
+          "message": "Fix the path length limit and update sefs",
+          "timestamp": "2022-08-15T09:51:09+08:00",
+          "tree_id": "f8f95c3863faf1a4fab6802ddbc05b0797da90ea",
+          "url": "https://github.com/occlum/ngo/commit/49a55608604681e7fbf1d9ad49cce3623b9b1b1e"
+        },
+        "date": 1660546033865,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2664,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2664,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      }
+    ]
+  }
+}
