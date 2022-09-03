b437d96de5edc8307f22ab1258fdd7f00d1b37ff
github-action-benchmark <github@users.noreply.github.com>
2022-09-04 06:09:08 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for b57c5db2ffc4af03724574ed069026cc207a80ba

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662156950905,
+  "lastUpdate": 1662242948285,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -996,6 +996,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 186.54,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "LI Qing",
+            "username": "liqinggd",
+            "email": "geding.lq@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "b57c5db2ffc4af03724574ed069026cc207a80ba",
+          "message": "Enable the page cache for async fs",
+          "timestamp": "2022-08-16T06:52:49Z",
+          "url": "https://github.com/occlum/ngo/commit/b57c5db2ffc4af03724574ed069026cc207a80ba"
+        },
+        "date": 1662242947323,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.07,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 32.42,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 674.15,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 193.38,
             "unit": "ms",
             "extra": "per95"
           }
