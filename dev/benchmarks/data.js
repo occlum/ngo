509e83539e9a90f51c9fc634e88b9d930fef3005
github-action-benchmark <github@users.noreply.github.com>
2022-09-10 06:09:14 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 43af5a2cae526e148f597f4a3ba1a81da02a2541

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662675341127,
+  "lastUpdate": 1662761354789,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -1476,6 +1476,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 204.11,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "1498430017@qq.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "43af5a2cae526e148f597f4a3ba1a81da02a2541",
+          "message": "[block-device] Fix a minor bug in BlockDeviceAsFile",
+          "timestamp": "2022-09-02T07:40:36Z",
+          "url": "https://github.com/occlum/ngo/commit/43af5a2cae526e148f597f4a3ba1a81da02a2541"
+        },
+        "date": 1662761353778,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.07,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 33.32,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 678.88,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 196.89,
             "unit": "ms",
             "extra": "per95"
           }
