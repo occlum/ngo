f4ceb0b6622602b15937afe43553ecd2d0942948
github-action-benchmark <github@users.noreply.github.com>
2022-09-20 06:15:33 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 31c5b20d4bd84e2ff8a6f3a93d9a8c4da29070aa

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663625357370,
+  "lastUpdate": 1663625733326,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2310,6 +2310,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 179.94,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "31c5b20d4bd84e2ff8a6f3a93d9a8c4da29070aa",
+          "message": "[uds] Add test case for sendmsg/recvmsg",
+          "timestamp": "2022-09-14T11:00:40Z",
+          "url": "https://github.com/occlum/ngo/commit/31c5b20d4bd84e2ff8a6f3a93d9a8c4da29070aa"
+        },
+        "date": 1663625732174,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.07,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 40.56,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 666.35,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 219.36,
             "unit": "ms",
             "extra": "per95"
           }
