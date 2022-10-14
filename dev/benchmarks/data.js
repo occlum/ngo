ba1429004ed3dae7e869a362996e997f36502c87
github-action-benchmark <github@users.noreply.github.com>
2022-10-15 06:13:14 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 8dff1e7e9b79d50706b5453b677b016b246f4462

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1665699837247,
+  "lastUpdate": 1665785594294,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -4276,6 +4276,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 173.58,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "8dff1e7e9b79d50706b5453b677b016b246f4462",
+          "message": "[test] Add test cases for epoll\n\nCo-authored-by: rduan@apache.org",
+          "timestamp": "2022-09-28T03:35:18Z",
+          "url": "https://github.com/occlum/ngo/commit/8dff1e7e9b79d50706b5453b677b016b246f4462"
+        },
+        "date": 1665785593231,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.07,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 32.56,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 658.45,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 193.38,
             "unit": "ms",
             "extra": "per95"
           }
