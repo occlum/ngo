0bc9a887311923d2995b6886f0623a1412c56a58
github-action-benchmark <github@users.noreply.github.com>
2022-10-30 06:08:47 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for c1892f1c465d5ba090ccce9fba34f8551f15f4f8

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666995273299,
+  "lastUpdate": 1667081326993,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2210,6 +2210,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 3511,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "c1892f1c465d5ba090ccce9fba34f8551f15f4f8",
+          "message": "Update CI to use occlum/ngo image",
+          "timestamp": "2022-10-17T08:24:13Z",
+          "url": "https://github.com/occlum/ngo/commit/c1892f1c465d5ba090ccce9fba34f8551f15f4f8"
+        },
+        "date": 1667081325914,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 3708,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 3707,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
