d9bdea526c5e35d05b609691f9606c71ca6b2b66
github-action-benchmark <github@users.noreply.github.com>
2022-11-05 06:58:03 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for ae4e3d61d67a39bfe8d9eaac5a04f7e9c1248ed2

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667602318491,
+  "lastUpdate": 1667602683142,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2380,6 +2380,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 3709,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Song Shaowei",
+            "username": "lucassong-mh",
+            "email": "1498430017@qq.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "ae4e3d61d67a39bfe8d9eaac5a04f7e9c1248ed2",
+          "message": "[benchmark ci] Add fio to benchmark ci",
+          "timestamp": "2022-11-01T06:34:29Z",
+          "url": "https://github.com/occlum/ngo/commit/ae4e3d61d67a39bfe8d9eaac5a04f7e9c1248ed2"
+        },
+        "date": 1667602681980,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 3508,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 3508,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
