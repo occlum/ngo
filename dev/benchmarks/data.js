218805a08aa4b4e869baea3de7241bcdd1726086
github-action-benchmark <github@users.noreply.github.com>
2022-11-12 06:47:39 +0800

    add FIO Benchmark (customBiggerIsBetter) benchmark result for 338c90c8b8d6c4a856bce8928fd31cc88d8af799

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668121331772,
+  "lastUpdate": 1668206858994,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -6440,6 +6440,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 177,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "338c90c8b8d6c4a856bce8928fd31cc88d8af799",
+          "message": "[ci] Enlarge vault wait time",
+          "timestamp": "2022-11-11T08:30:28Z",
+          "url": "https://github.com/occlum/ngo/commit/338c90c8b8d6c4a856bce8928fd31cc88d8af799"
+        },
+        "date": 1668206857579,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 40.9,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 27.4,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 285,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 168,
             "unit": "MiB/s",
             "extra": "randread"
           }
