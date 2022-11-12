0b1e1f3a81e9a5c034c0b142a7a57b3f77345755
github-action-benchmark <github@users.noreply.github.com>
2022-11-13 06:10:46 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 338c90c8b8d6c4a856bce8928fd31cc88d8af799

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668207780205,
+  "lastUpdate": 1668291046450,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2652,6 +2652,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 3575,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "338c90c8b8d6c4a856bce8928fd31cc88d8af799",
+          "message": "[ci] Enlarge vault wait time",
+          "timestamp": "2022-11-11T08:30:28Z",
+          "url": "https://github.com/occlum/ngo/commit/338c90c8b8d6c4a856bce8928fd31cc88d8af799"
+        },
+        "date": 1668291045341,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 3583,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 3582,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
