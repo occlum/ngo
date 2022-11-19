b976f4fe11b235a72edb374e1af248aa079d3e40
github-action-benchmark <github@users.noreply.github.com>
2022-11-20 06:47:30 +0800

    add FIO Benchmark (customBiggerIsBetter) benchmark result for 13045ade447b6f7a193f17aec72964d46e82a7d7

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668812658073,
+  "lastUpdate": 1668898050636,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -7448,6 +7448,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 189,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "13045ade447b6f7a193f17aec72964d46e82a7d7",
+          "message": "Clear msghdr flags to 0 after recvmsg",
+          "timestamp": "2022-10-27T07:34:19Z",
+          "url": "https://github.com/occlum/ngo/commit/13045ade447b6f7a193f17aec72964d46e82a7d7"
+        },
+        "date": 1668898049221,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 41.1,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 28.2,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 283,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 187,
             "unit": "MiB/s",
             "extra": "randread"
           }
