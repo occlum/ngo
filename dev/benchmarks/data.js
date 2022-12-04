857ff548cfdb658042d8a29520e445de9a583e8b
github-action-benchmark <github@users.noreply.github.com>
2022-12-05 06:47:51 +0800

    add FIO Benchmark (customBiggerIsBetter) benchmark result for c996f9709965a69a59673029c0757315055f1a90

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670192106084,
+  "lastUpdate": 1670194071773,
   "repoUrl": "https://github.com/occlum/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -9418,6 +9418,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 186,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "LI Qing",
+            "username": "liqinggd",
+            "email": "geding.lq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "c996f9709965a69a59673029c0757315055f1a90",
+          "message": "[docs] Update some fs documents",
+          "timestamp": "2022-12-01T03:05:09Z",
+          "url": "https://github.com/occlum/ngo/commit/c996f9709965a69a59673029c0757315055f1a90"
+        },
+        "date": 1670194070357,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 50.4,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 42.3,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 287,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 187,
             "unit": "MiB/s",
             "extra": "randread"
           }
